6dcaefcb8c24249cfb39d872b6c69611491f3594
Scott Bartell <sbartell@uci.edu>
2012-12-18 00:00:00 +0000

    version 0.6

---
+++ R/colormap.R
@@ -0,0 +1,73 @@
+colormap <-
+function(modgamobj, map=NULL, add=F, mapmin=NULL, mapmax=NULL, ptsize=0.9, alpha=0.05) {
+	results = cbind(modgamobj$grid,modgamobj$OR)
+	if (!is.null(modgamobj$pointwise)) results = cbind(results,modgamobj$pointwise)
+	if (is.null(mapmin)) mapmin=round(min(results[,3]),2)
+	if (is.null(mapmax)) mapmax=round(max(results[,3]),2)
+	dataXmin=min(results[,1])
+	dataXmax=max(results[,1])
+	dataYmin=min(results[,2])
+	dataYmax=max(results[,2])
+	offsetX=(dataXmax-dataXmin)/5
+	offsetY=(dataYmax-dataYmin)/5
+	qu = seq(mapmin,mapmax,length=2251)
+	cp = rainbow(2252,start=0,end=0.66)
+	col.seq = rev(cp)
+	grad = cut(results[,3],breaks=c(0,qu,Inf),labels=F)
+	if (add==T) points(modgamobj$grid,col=col.seq[grad],pch=15,cex=ptsize) else {
+		par(mai=c(0,0,0.75,0))
+		plot(modgamobj$grid,col=col.seq[grad],pch=15,cex=ptsize,type="p",xlim=c(dataXmin,dataXmax),
+			 ylim=c((dataYmin-0.8*offsetY),dataYmax),ann=F,axes=F)
+	}
+	if (!is.null(map)) {
+		if (class(map)=="SpatialPolygonsDataFrame") plot(map, border=gray(0.3), add=T) else {
+			if (class(map)!="map") warning("map class not recognized--attempting generic plot")
+			lines(map,col=gray(0.3),type="l")
+		}
+	}
+	# CONTOUR LINES
+	if (length(results)>=4) {	# if column exists for permutation ranks
+		df=cbind(results[(order(results[,2])),1:2],results[(order(results[,2])),4])
+		names(df)[3]="perm"
+		df = df[!is.na(df$perm),]
+		cx=as.matrix(unique(df[(order(df[,1])),1]))
+		cy=as.matrix(unique(df[,2]))
+		cz=as.matrix(reshape(df,v.names=names(df)[3],idvar=names(df)[1],timevar=names(df)[2],direction="wide")[,-1])
+		cz=cz[order(unique(df[,1])),]
+		levs = c(alpha/2,1-alpha/2)
+		contour(x=cx,y=cy,z=cz,levels=levs,color="black", drawlabels=FALSE, lwd=3, lty="twodash", add=TRUE)
+	}
+
+	# LEGEND
+	leglab = "Odds Ratios"
+	if (!is.null(modgamobj$m)) leglab = paste(modgamobj$m,leglab)
+	len=(dataXmax-dataXmin)*2/3
+	stop=trunc(len*(1-mapmin)/(mapmax-mapmin)/(len/2252))
+	points(dataXmin+(1:2252*len/2252),rep(dataYmin-0.5*offsetY,2252),cex=1.6,col=cp[2253-1:2252],pch=15)
+	points(dataXmin+len*(1-mapmin)/(mapmax-mapmin),(dataYmin-0.5*offsetY),cex=.8,col="black", pch="|", lwd=2)
+	text(x=dataXmin,y=(dataYmin-0.5*offsetY),pos=1,labels=format(mapmin,nsmall=2),cex=.8)
+	text(x=dataXmin+len,y=(dataYmin-0.5*offsetY),pos=1,labels=format(mapmax,nsmall=2),cex=.8)
+	text(x=dataXmin+len*(1-mapmin)/(mapmax-mapmin),y=(dataYmin-0.5*offsetY),pos=1,labels="1.00",cex=.8)
+	text(x=dataXmin+len/2,y=(dataYmin-0.5*offsetY),pos=3,labels=leglab,cex=1) 
+
+	# NORTH ARROW
+	points(dataXmax,dataYmin-0.65*offsetY,cex=1.2,col="black", pch="|", lwd=2)
+	points(dataXmax-0.01*offsetX,dataYmin-0.5*offsetY,cex=1.2,col="black", bg="black", pch=24)
+	text(x=dataXmax,y=(dataYmin-0.5*offsetY),"N",cex=1,pos=3)
+
+	# SCALE BAR
+	if (is.null(map)==F & class(map)=="SpatialPolygonsDataFrame") {
+		d=0.17*(dataXmax - dataXmin)
+		if (d>1000) {d=d/1000; units="km"} else {units="m"}
+		d=ceiling(d)
+		d=5*(d%/%5)
+		f=(d*1000)/(dataXmax - dataXmin)	
+		d=paste(as.character(format(d,digits=2)),units)
+		text(x=dataXmin+1.125*len+0.5*f*(dataXmax - dataXmin),y=(dataYmin-0.5*offsetY),d,cex=1,pos=1)
+		points(dataXmin+1.125*len,dataYmin-0.5*offsetY,cex=1.2,col="black", pch="|", lwd=2)
+		points(dataXmin+1.125*len+f*(dataXmax - dataXmin),dataYmin-0.5*offsetY,cex=1.2,col="black",
+			   pch="|", lwd=2)
+		lines(c(dataXmin+1.125*len,dataXmin+1.125*len+f*(dataXmax - dataXmin)),
+			  c(dataYmin-0.5*offsetY,dataYmin-0.5*offsetY),col="black",lwd=3)
+	}
+}
---
+++ R/modgam.R
@@ -0,0 +1,78 @@
+modgam <-
+function (rdata, rgrid, permute=0, m="adjusted", sp=NULL, keep=F, verbose=T, ...) {
+	x=length(rdata)
+	if (x < 3) stop("rdata must include at least 3 columns")
+	if (tolower(m)=='adjusted') {
+		m = "Adjusted"
+		if (x==3) stop("cannot run adjusted model without additional covariates in rdata")
+		# fill in median values (or first mode for factors) for any missing covariates in rgrid 
+		addvars = names(rdata)[!names(rdata) %in% names(rgrid)][-1]
+		if (length(addvars)>0) {
+			for (i in addvars) {
+				if (is.factor(rdata[,i]))  rgrid[i]=factor(names(which.max(table(rdata[,i]))),levels=levels(rdata[,i])) else
+					rgrid[i]=quantile(rdata[,i],0.5)		
+				cat(paste("GAM predictions will use ",i," = ",rgrid[1,i]," at all grid points.",sep=""),fill=T) 
+			}
+		}
+	} else
+	if (tolower(m)=='unadjusted' | tolower(m)=='crude') {
+		x = 3
+		m = "Unadjusted"
+	} else
+		stop(paste("model type",m,"not recognized"))
+	if (is.null(sp)) sp=optspan(rdata,m)
+	# Creates the crude or adjusted model using the names of variables in the data set, rgrid. Also creates the reduced model
+	#	omitting the smooth term
+	if (x == 3) {fmla = as.formula(paste(names(rdata)[1], paste("lo(",paste(names(rdata)[2:3], collapse=","),",span=",sp,")"), sep="~"));
+	fmla.0 = as.formula(paste(names(rdata)[1], paste("1"), sep="~"))} else 
+	{fmla = as.formula(paste(names(rdata)[1], paste("lo(",paste(names(rdata)[2:3], collapse=","),",span=",sp,")+",paste(names(rdata)[-(1:3)], collapse= "+")), sep="~"));
+	fmla.0 = as.formula(paste(names(rdata)[1], paste(names(rdata)[-(1:3)], collapse= "+"), sep="~"))}
+	if (verbose) {
+		cat(paste("The ",tolower(m)," model is: ",sep=""),fill=T) 
+		print(fmla,showEnv=F)
+	} 
+	model=gam(fmla, family = binomial(logit), data = rdata, ...) 
+	model.0=gam(fmla.0, family = binomial(logit), data = rdata, ...)
+	nullmod=ifelse (x == 3, mean(predict.gam(model.0)), mean(predict.gam(model.0,rgrid))) # aspatial model for calculating ORs
+
+	# Calculates the predicted log odds
+	origresults = predict.gam(model,rgrid)					# needs rgrid to include covariates 
+	# Converts from log odds to odds ratios (ORs) using the whole study area as the reference, 
+	# dividing the odds at each grid point by the odds calculated by the reduced model omitting the smoothing term.
+	# store grid, ORs, and call arguments in results list  
+	results=list(grid=rgrid[,1:2],OR=as.vector(exp(origresults-nullmod)),m=m,span=sp,gamobj=model)
+
+	# Runs permutation tests
+	if (permute>0) { 	##PERMUTATION TEST##
+		n=length(rgrid[,1])
+		nobs = length(rdata[,1])
+		permresults=NULL 						# for permuted OR estimates
+		ptranks=rep(1, n)						# for pointwise ranks
+		devstat=rep(NA, permute)					# for global test
+		devstat[1]=data.frame(anova(model.0, model, test="Chi"))[2,4]	# deviance statistic for original data
+		coords=rdata[,2:3]  			
+		m.data=rdata
+		for (i in 2:permute) {
+			index=sample(1:nobs, replace=F)
+			m.data[,2:3]=coords[index,]				# randomly reassign individuals to the eligible residences
+			# For each permutation, we run the GAM (using a fixed span size)
+			m.gam=gam(fmla, family = binomial, data = m.data, ...)
+			devdata=data.frame(anova(model.0, m.gam, test="Chi"))
+			devstat[i]=devdata[2,5]						# deviance statistic for the model from the permuted data
+			tempresults=predict.gam(m.gam, rgrid)		# estimate log odds at each point from the permuted data
+			ptranks = ptranks + (origresults > tempresults) 
+			if (keep) permresults=cbind(permresults,tempresults)	# add log odds estimate vector to stored results		 
+			if (verbose && i%%10==0) cat(paste("Permutation",i,"of",permute),fill=TRUE)
+			}
+		devglobp=(permute-rank(devstat)[1])/permute			# global p-value based on permuted deviance statistic
+        globprint = if (devglobp == 0) paste("<",round(1/permute,3),sep="") else devglobp
+		cat(paste("The global statistic for the ",tolower(m)," model is ",globprint,sep=""), fill=TRUE)
+		results$global = devglobp								# save global percentile rank
+		results$pointwise = as.vector(ptranks/permute)			# save pointwise percentile ranks
+		if (keep) {
+			results$permutations=exp(permresults-nullmod)		# save permuted odds ratios
+			results$globaldevs=devstat							# save permuted global deviance statistics
+			}
+		}
+	return(results)
+	}
